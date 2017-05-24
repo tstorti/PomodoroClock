87ebf40837cc3cdaa69d778d1a1f69e113c95f7a
Tony Storti <tonystorti@gmail.com>
2017-05-24 17:07:21 -0500

    added functionality for two time intervals and start/stop

--- javascript/logic.js
+++ javascript/logic.js
@@ -1,31 +1,68 @@
 $(document).ready(function() {
    
 	//set starting time and separate into minutes and seconds for display
-	var startTime = 2.5
-	var startTimeMin = Math.floor(startTime);
-	var startTimeSec = Math.floor(60*((startTime-startTimeMin) % 1));
-	
+	var status=0;
+	var startTime1 = 3;
+	var startTime2 = 2;
+	var timeInterval1=null;
+	var timeInterval2=null;
+
 	//output starting time
-	$("#startingTime").text(startTimeMin+":"+startTimeSec);
-	
-	//start countdown when clicking start button
-	$("#start").on("click",function(){
-		startTimer();
+	$("#startingTime1").text(formatTime(startTime1));
+	$("#startingTime2").text(formatTime(startTime2));
+
+	$("#timeLeft1").attr("data-time", startTime1);
+	$("#timeLeft2").attr("data-time", startTime2);
+
+	//start/stop countdown when clicking buttons
+	$("#start1").on("click",function(){
+		startTimer("#timeLeft1");
+	});
+	$("#stop1").on("click",function(){
+		stopTimer("#timeLeft1");
+	});
+
+	$("#start2").on("click",function(){
+		startTimer("#timeLeft2");
+	});
+	$("#stop2").on("click",function(){
+		stopTimer("#timeLeft2");
 	});
 
 	//set time interval for clock updates (using seconds)
-	function startTimer() {
-		var timeInterval = window.setInterval(updateTimerOutput, 1000);
+	function startTimer(target) {
+		if(target==="#timeLeft1"){	
+			timeInterval1=window.setInterval(function(){updateTimerOutput(target)},1000);
+		}
+		if(target==="#timeLeft2"){	
+			timeInterval2=window.setInterval(function(){updateTimerOutput(target)},1000);
+		}
 	}
 	
-	//update clock output every time interval passes
-	function updateTimerOutput() {
-		startTime = startTime-(1/60);
-		startTimeMin = Math.floor(startTime);
-		startTimeSec = Math.floor(60*((startTime-startTimeMin) % 1));
-	
-		$("#timeLeft").text(Math.floor(startTime)+":"+startTimeSec);
+	function stopTimer(target){
+		if(target==="#timeLeft1"){	
+			window.clearInterval(timeInterval1);
+		}
+		if(target==="#timeLeft2"){	
+			window.clearInterval(timeInterval2);
+		}
+	}
+	function test(){
+		$("#test").append("interval test");
+	}
 
+	//update clock output every time interval passes
+	function updateTimerOutput(target) {
+		var adjTime = $(target).attr("data-time");
+		adjTime -= (1/60);
+		$(target).attr("data-time", adjTime);
+		$(target).text(formatTime(adjTime));
+	}
+	function formatTime(num){
+		var startTimeMin = Math.floor(num);
+		var startTimeSec = Math.floor(60*((num-startTimeMin) % 1));
+		startTimeSec = ("0" + startTimeSec).slice(-2);
+		return (startTimeMin+":"+startTimeSec);
 	}
 
 });
